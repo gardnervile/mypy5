56a7370aea3b69406b1bcf7c90c20b60c7c6cf3a
gardnervile <lordy1jo@gmail.com>
2024-10-15 23:17:16 +0500

    Initial commit

---
+++ mypy5.py
@@ -0,0 +1,79 @@
+from faker import Faker
+import file_operations
+import random
+import os
+
+
+REPLACEMENT_DICT = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+OUTPUT_DIRECTORY = "game_cards"
+
+
+def generate_card(i):
+    fake = Faker("ru_RU")
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    job = fake.job()
+    city = fake.city()
+
+    skills = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
+              "Стремительный удар", "Кислотный взгляд", "Тайный побег",
+              "Ледяной выстрел", "Огненный заряд"]
+    selected_skills = random.sample(skills, 3)
+
+    replaced_skills = []
+    for skill in selected_skills:
+        replaced_skill = ''.join(REPLACEMENT_DICT.get(char, char) for char in skill)
+        replaced_skills.append(replaced_skill)
+
+    context = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "job": job,
+        "town": city,
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": replaced_skills[0],
+        "skill_2": replaced_skills[1],
+        "skill_3": replaced_skills[2]
+    }
+
+    file_name = os.path.join(OUTPUT_DIRECTORY, "result{}.svg".format(i))
+    file_operations.render_template("charsheet.svg", file_name, context)
+
+def main():
+    os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
+    for i in range(1, 11):
+        generate_card(i)
+
+
+if __name__ == '__main__':
+    main()
